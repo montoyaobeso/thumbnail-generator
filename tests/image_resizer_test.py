ba0da4ea252472f22a1819564e4b5b09544a4922
Abraham Montoya <montoyaobeso@gmail.com>
2024-05-08 14:55:20 -0700

    tests: restore test

--- tests/image_resizer_test.py
+++ tests/image_resizer_test.py
@@ -20,7 +20,7 @@ class TestImageResizer(TestCase):
         result = image.get_output_image()
 
         # Assert
-        self.assertTrue(result.size == True)
+        self.assertTrue(result.size == (128, 128))
 
     def test_get_original_image_size(self):
         # Arrange
